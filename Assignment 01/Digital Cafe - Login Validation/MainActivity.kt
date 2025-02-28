6b5eca17e8e8815b9333ffc029ffc17cc34bbeaf
Nikilashree <220701186@rajalakshmi.edu.in>
2025-02-28 20:35:40 +0530

    Create MainActivity.kt

---
+++ Assignment 01/Digital Cafe - Login Validation/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.loginvalidation
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginvalidation.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val etText: EditText = findViewById(R.id.etText)
+        val etPass: EditText = findViewById(R.id.etPass)
+        val btDisplay: Button = findViewById(R.id.btDisplay)
+        var errorMessage = ""
+        btDisplay.setOnClickListener {
+            val email = etText.text.toString()
+            val password = etPass.text.toString()
+            errorMessage="Successfully Logged In"
+
+            if (password.length < 8 || !password.any { it.isUpperCase() } || !password.any { it.isLowerCase() } || !password.any { it.isDigit() } || !password.any {
+                    "!@#\$%^&*()-_+=<>?/".contains(it)
+                }) {
+                 errorMessage = "Invalid Password"
+            }
+            if (!email.any { "!@#\$%^&*()-_+=<>?/".contains(it) }) {
+                 errorMessage = "Invalid Email"
+            }
+
+            val alertDialog = AlertDialog.Builder(this)
+                .setTitle("MAD Lab")
+                .setMessage(errorMessage)
+                .setPositiveButton("Ok") { dialog, which ->
+                    Toast.makeText(this, "You clicked Ok", Toast.LENGTH_LONG).show()
+                }
+                .setNeutralButton("Cancel") { dialog, which ->
+                    Toast.makeText(this, "You clicked Cancel", Toast.LENGTH_LONG).show()
+                }
+                .create()
+            alertDialog.show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
